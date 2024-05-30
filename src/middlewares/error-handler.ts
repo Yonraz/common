421bb73987e7d39f1512aabc1c61aa78103a35c3
yonraz <yraz530@gmail.com>
2024-05-30 16:46:58 +0300

    added error logging to errorhandler.ts

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -13,6 +13,7 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
+  console.error(err);
   const defaultError: ErrorResponse[] = [{ message: "Something went wrong" }];
   res.status(400).send(defaultError);
 };
