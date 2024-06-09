82f8d910f1ae489a0fdb9a255d291eddb70c6113
Yonraz <99009434+Yonraz@users.noreply.github.com>
2024-06-09 12:52:00 +0300

    versions

--- src/events/TicketUpdatedEvent.ts
+++ src/events/TicketUpdatedEvent.ts
@@ -1,11 +1,12 @@
 import { Topics } from "./TopicEnum";
 
 export interface TicketUpdatedEvent {
-    topic: Topics.TicketUpdated;
-    data: {
-        id: string;
-        title: string;
-        price: number;
-        userId: string;
-    };
+  topic: Topics.TicketUpdated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+    version: number;
+  };
 }
